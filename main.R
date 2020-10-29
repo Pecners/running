777dc098f77628c052192517d0f10e09a8f99c98
Spencer Schien <spencer.schien@cityforwardcollective.org>
2020-10-29 10:25:01 -0500

    Adding modeling to running time

--- main.R
+++ main.R
@@ -21,11 +21,27 @@ pr_5k <- full_times %>%
   filter(distance == 5) %>%
   summarise(fastest = min(time_5k)) %>%
   .[[1]]
+
+mod_df <- full_times %>%
+  filter(distance == 5 & year(date) > 2019) %>%
+  mutate(adj_date = as.numeric(date) - min(as.numeric(date)))
+
+mod <- lm(adj_date ~ time_5k, data = mod_df)
+
+summary(mod)
+
+goal_date <- min(mod_df$date) + predict(mod, data.frame(time_5k = 1606))
   
 full_times %>%
   filter(distance == 5 & date > ymd("2020-01-01")) %>%
   ggplot(aes(date, time_5k)) +
   geom_point() +
+  annotate(geom = "text", label = paste0("Predicted completion date:\n", goal_date),
+           x = ymd(goal_date), y = 2000) +
+  scale_x_date(limits = c(min(mod_df$date) - 7, goal_date + 14)) +
   geom_hline(linetype = 3, yintercept = pr_5k) +
-  scale_y_time(limits = c(60 * 25, 60* 35), breaks = 60 * seq(25, 35, 5)) +
-  theme_minimal()
+  scale_y_time(limits = c(60 * 25, 60 * 35), breaks = 60 * seq(25, 35, 5)) +
+  theme_minimal() +
+  geom_smooth(method = "lm", se = FALSE)
+
+
